6297ecb8d15b637ffce642cbf9cb0a613958c1df
aldaine-AG <aldaineclarke1@gmail.com>
2023-01-22 08:24:26 -0500

    created about page and links navigation from the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Pages/about/about.component';
 import { HomeComponent } from './Pages/home/home.component';
 
 const routes: Routes = [
   {
     path: 'home',component: HomeComponent
+  }, 
+  {
+    path: 'about',component: AboutComponent
   }, 
   {
     path: '',redirectTo: "/home", pathMatch:"full"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { AboutComponent } from './Pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
