335556a2ba0b928c195c94e7a53065219ca52091
aldaine-AG <aldaineclarke1@gmail.com>
2023-01-26 11:09:12 -0500

    added indicator to the slider

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,14 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+  @ViewChild("carousel_wrapper") carouselSlider !: ElementRef;
+  @ViewChild("prev") prevArrow !: ElementRef;
+  @ViewChild("next") nextArrow !: ElementRef;
+
+  projects = [
+    {
+      img:"/assets/HMDC/large-home-g6c9303983_1280.jpg",
+      name:"Two Storey Home",
+      duration:"8 Months",
+      work: "General Construction"
+    },
+    {
+      img:"/assets/HMDC/architecture-gba45d817b_1920.jpg",
+      name:"Two Storey Home",
+      duration:"8 Months",
+      work: "General Construction"
+    },
+    {
+      img:"/assets/HMDC/restored-home-gd237d4e09_1920.jpg",
+      name:"Township Home",
+      duration:"3 Months",
+      work: "Building Mentenace"
+    },
+    {
+      img:"/assets/HMDC/construction-g68122eaac_1920.jpg",
+      name:"Two Storey Home",
+      duration:"8 Months",
+      work: "General Construction"
+    },
+    {
+      img:"/assets/HMDC/sa-rapita-gcf8a1a55e_1920.jpg",
+      name:"Two Storey Home",
+      duration:"8 Months",
+      work: "General Construction"
+    },
+    {
+      img:"/assets/HMDC/large-home-g6c9303983_1280.jpg",
+      name:"Two Storey Home",
+      duration:"8 Months",
+      work: "General Construction"
+    }
+  ];
+
+  slider!:HTMLElement;
+  nextBtn!: HTMLElement;
+  prevBtn!:HTMLElement
+  index = 1;
+  slides:HTMLElement[] = []; 
+  translateByVal = 0;
   services = [
     {
       title: "General Construction",
@@ -53,6 +102,62 @@ export class HomeComponent implements OnInit {
   active = false;
   toggleNav(){
     this.active = !this.active;
+  }
+
+  ngAfterViewInit(): void {
+    this.slider = this.carouselSlider.nativeElement;
+
+    this.nextBtn = this.nextArrow.nativeElement;
+    this.prevBtn = this.prevArrow.nativeElement;
+    Math.max()
+    this.slider.querySelectorAll(".carousel__slide").forEach((item, index)=>{
+      if(index == 0){
+        item.classList.add("prev");
+      }else if(index == this.index){
+        item.classList.add("active");
+      }else{
+        item.classList.add("next");
+      }
+      this.translateByVal = Math.max(this.translateByVal, item.clientWidth)
+      this.slides.push(item as HTMLElement);
+    });
+
+
+  }
+
+
+  nextSlide():void {
+    console.log(this.index)
+    if(this.index >= 0){
+      this.prevBtn.style.display = "flow-root";
+    }if(this.index >= 4){
+      this.nextBtn.style.display = "none";
+    }else if(this.index > 5){return};
+   this. slider.style.transform = `translateX(-${this.translateByVal * this.index}px)`;
+    this.slides[this.index].classList.replace("active", "prev");
+    this.index++;
+    this.slides[this.index].classList.replace("next", "active");
+
+    // console.log(slider.scrollLeft += window.innerWidth / 3);
+  }
+  resetActiveSlide(slides:HTMLElement[]){
+    slides.forEach((slide)=>{
+      slide.classList.remove("active");
+    })
+  }
+
+  previousSlide():void{
+    console.log(this.index);
+    if(this.index <= 1){
+      this.prevBtn.style.display = "none";
+    }else if(this.index <= 5){
+      this.nextBtn.style.display = "flow-root"
+    }else{
+      return
+    }
+    this. slider.style.transform = `translateX(${-(this.translateByVal) * (--this.index -1)}px)`;
+    this.slides[this.index+1].classList.replace("active", "next");
+    this.slides[this.index].classList.replace("prev", "active");
   }
 
 }
