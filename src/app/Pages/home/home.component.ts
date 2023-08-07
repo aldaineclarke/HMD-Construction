01c9c550ddbc410db2e1c33278dd6580aec2c226
Aldaine-Legion <vainedev@gmail.com>
2023-08-06 20:03:14 -0500

    removed the absolute path from images in the array in home component

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -18,37 +18,37 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   projects = [
     {
-      img:"/assets/HMDC/large-home-g6c9303983_1280.jpg",
+      img:"assets/HMDC/large-home-g6c9303983_1280.jpg",
       name:"Two Storey Home",
       duration:"8 Months",
       work: "General Construction"
     },
     {
-      img:"/assets/HMDC/architecture-gba45d817b_1920.jpg",
+      img:"assets/HMDC/architecture-gba45d817b_1920.jpg",
       name:"Two Storey Home",
       duration:"8 Months",
       work: "General Construction"
     },
     {
-      img:"/assets/HMDC/restored-home-gd237d4e09_1920.jpg",
+      img:"assets/HMDC/restored-home-gd237d4e09_1920.jpg",
       name:"Township Home",
       duration:"3 Months",
       work: "Building Mentenace"
     },
     {
-      img:"/assets/HMDC/construction-g68122eaac_1920.jpg",
+      img:"assets/HMDC/construction-g68122eaac_1920.jpg",
       name:"Two Storey Home",
       duration:"8 Months",
       work: "General Construction"
     },
     {
-      img:"/assets/HMDC/sa-rapita-gcf8a1a55e_1920.jpg",
+      img:"assets/HMDC/sa-rapita-gcf8a1a55e_1920.jpg",
       name:"Two Storey Home",
       duration:"8 Months",
       work: "General Construction"
     },
     {
-      img:"/assets/HMDC/large-home-g6c9303983_1280.jpg",
+      img:"assets/HMDC/large-home-g6c9303983_1280.jpg",
       name:"Two Storey Home",
       duration:"8 Months",
       work: "General Construction"
@@ -65,36 +65,36 @@ export class HomeComponent implements OnInit, AfterViewInit {
     {
       title: "General Construction",
       desc: "We have a wide array of contractor and workers with excellent workmanship to ensure your construction project is realized.",
-      img: "/assets/HMDC/ConstructionBlue.jpg"
+      img: "assets/HMDC/ConstructionBlue.jpg"
     },
     {
       title: "Equipment Rental",
       desc: "There is no need to purchase equipment that you may only use once. We rent all construction equipment , both small and heavy duty.",
-      img: "/assets/HMDC/Vibratory-Compactor-GC-1024x683.jpeg"
+      img: "assets/HMDC/Vibratory-Compactor-GC-1024x683.jpeg"
 
     },
     {
       title: "Construction Planning & Design",
       desc: "Drawing up plans and models for your dream home, we aim to ensure that you have a clear idea of how the finished product will turn out ",
-      img: "/assets/HMDC/images.jfif"
+      img: "assets/HMDC/images.jfif"
 
     },
     {
       title: "Transportation Solution",
       desc: "Moving into your new home could never have been this easy. Take advantage of our moving and transportation  service.",
-      img: "/assets/HMDC/moving truck.jpg"
+      img: "assets/HMDC/moving truck.jpg"
 
     },
     {
       title: "Legal Consultation",
       desc: "Our legal team will happily assist with beneficial advice and service to assist with purchase and selling you construction project.",
-      img: "/assets/HMDC/construction-consulting-1.jpg"
+      img: "assets/HMDC/construction-consulting-1.jpg"
 
     },
     {
       title: "Building Maintenance",
       desc: "We also perform construction maintenance to already created projects, make the project looks as good as new.",
-      img: "/assets/HMDC/maintenance.jpg"
+      img: "assets/HMDC/maintenance.jpg"
 
     },
 
