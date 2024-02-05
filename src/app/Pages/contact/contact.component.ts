5b144d73523920d1abbf30c6291f09da81b4786a
Aldaine-Legion <vainedev@gmail.com>
2024-02-04 21:19:44 -0500

    validated forms for contact us page

--- src/app/Pages/contact/contact.component.ts
+++ src/app/Pages/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -9,31 +9,39 @@ import { FormBuilder, FormControl } from '@angular/forms';
 export class ContactComponent implements OnInit {
 
 
+  quoteFormSubmitted = false;
+  contactFormSubmitted = false;
+
   services = ["General Construction", "Equipment Rental", "Construction Planning and Design", "Transportation Solution", "Legal Consultation", "Building Maintenance"];
   quoteForm = this.fb.group({
-    name: new FormControl(""),
-    email: new FormControl(""),
-    phone: new FormControl(""),
-    message: new FormControl(""),
+    name: new FormControl("",[Validators.required, Validators.minLength(3)]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    phone: new FormControl("", [Validators.required]),
+    message: new FormControl("", [Validators.required, Validators.minLength(3)]),
     service: new FormControl(""),
   });
 
   contactForm = this.fb.group({
-    name: new FormControl(""),
-    email: new FormControl(""),
-    message: new FormControl(""),
+    name: new FormControl("",[Validators.required, Validators.minLength(3)]),
+    email: new FormControl("",[Validators.required, Validators.email]),
+    message: new FormControl("" ,[Validators.required, Validators.minLength(3)]),
   })
-
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   submitQuote(){
-    console.log(this.quoteForm.value)
+    this.quoteFormSubmitted = true;
+    if(this.quoteForm.valid){
+      alert("Successfully requested quote");
+    }
   }
   submitFeedback(){
-    console.log(this.contactForm.value)
+    this.contactFormSubmitted = true;
+    if(this.contactForm.valid){
+      alert("Contactform successfully submitted")
+    }
   }
 
 }
